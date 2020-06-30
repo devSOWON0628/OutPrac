807bf699b4bb424031fdb905adef101a234e505e
=devSOWON0628 <=s2018s29@e-mirim.hs.kr>
2020-06-30 14:56:30 +0900

    winform

---
+++ MenuStatusPrac/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace MenuStatusPrac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.정우오빠ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.영재오빠ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.동혁오빠ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저징ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.또저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내긔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.만복도ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.배고파ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보통ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.배불러ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.만복도ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem,
+            this.저징ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.끝내긔ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.정우오빠ToolStripMenuItem,
+            this.영재오빠ToolStripMenuItem,
+            this.동혁오빠ToolStripMenuItem});
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.열기ToolStripMenuItem.Text = "열기";
+            // 
+            // 정우오빠ToolStripMenuItem
+            // 
+            this.정우오빠ToolStripMenuItem.Name = "정우오빠ToolStripMenuItem";
+            this.정우오빠ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.정우오빠ToolStripMenuItem.Text = "정우오빠";
+            this.정우오빠ToolStripMenuItem.Click += new System.EventHandler(this.정우오빠ToolStripMenuItem_Click);
+            // 
+            // 영재오빠ToolStripMenuItem
+            // 
+            this.영재오빠ToolStripMenuItem.Name = "영재오빠ToolStripMenuItem";
+            this.영재오빠ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.영재오빠ToolStripMenuItem.Text = "영재오빠";
+            // 
+            // 동혁오빠ToolStripMenuItem
+            // 
+            this.동혁오빠ToolStripMenuItem.Name = "동혁오빠ToolStripMenuItem";
+            this.동혁오빠ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.동혁오빠ToolStripMenuItem.Text = "동혁오빠";
+            // 
+            // 저징ToolStripMenuItem
+            // 
+            this.저징ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장ToolStripMenuItem,
+            this.또저장ToolStripMenuItem});
+            this.저징ToolStripMenuItem.Name = "저징ToolStripMenuItem";
+            this.저징ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저징ToolStripMenuItem.Text = "저장";
+            this.저징ToolStripMenuItem.Click += new System.EventHandler(this.저징ToolStripMenuItem_Click);
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            // 
+            // 또저장ToolStripMenuItem
+            // 
+            this.또저장ToolStripMenuItem.Name = "또저장ToolStripMenuItem";
+            this.또저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.또저장ToolStripMenuItem.Text = "또저장";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 끝내긔ToolStripMenuItem
+            // 
+            this.끝내긔ToolStripMenuItem.Name = "끝내긔ToolStripMenuItem";
+            this.끝내긔ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내긔ToolStripMenuItem.Text = "끝내긔";
+            this.끝내긔ToolStripMenuItem.Click += new System.EventHandler(this.끝내긔ToolStripMenuItem_Click);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.편집ToolStripMenuItem.Text = "편집";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(67, 17);
+            this.toolStripStatusLabel1.Text = "김정우오빠";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Click += new System.EventHandler(this.toolStripProgressBar1_Click);
+            // 
+            // 만복도ToolStripMenuItem
+            // 
+            this.만복도ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.배고파ToolStripMenuItem,
+            this.보통ToolStripMenuItem,
+            this.배불러ToolStripMenuItem});
+            this.만복도ToolStripMenuItem.Name = "만복도ToolStripMenuItem";
+            this.만복도ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.만복도ToolStripMenuItem.Text = "만복도";
+            // 
+            // 배고파ToolStripMenuItem
+            // 
+            this.배고파ToolStripMenuItem.Name = "배고파ToolStripMenuItem";
+            this.배고파ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.배고파ToolStripMenuItem.Text = "배고파";
+            this.배고파ToolStripMenuItem.Click += new System.EventHandler(this.배고파ToolStripMenuItem_Click);
+            // 
+            // 보통ToolStripMenuItem
+            // 
+            this.보통ToolStripMenuItem.Name = "보통ToolStripMenuItem";
+            this.보통ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.보통ToolStripMenuItem.Text = "보통";
+            this.보통ToolStripMenuItem.Click += new System.EventHandler(this.보통ToolStripMenuItem_Click);
+            // 
+            // 배불러ToolStripMenuItem
+            // 
+            this.배불러ToolStripMenuItem.Name = "배불러ToolStripMenuItem";
+            this.배불러ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.배불러ToolStripMenuItem.Text = "배불러";
+            this.배불러ToolStripMenuItem.Click += new System.EventHandler(this.배불러ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저징ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 정우오빠ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 영재오빠ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 동혁오빠ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 또저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 끝내긔ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem 만복도ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 배고파ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보통ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 배불러ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ MenuStatusPrac/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuStatusPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 저징ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 정우오빠ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("정우오빠");
+        }
+
+        private void 끝내긔ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 배고파ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "배고파ㅜㅜ";
+            toolStripProgressBar1.Value = 10;
+        }
+
+        private void 보통ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "그냥저냥";
+            toolStripProgressBar1.Value = 60;
+
+        }
+
+        private void 배불러ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "배불로";
+            toolStripProgressBar1.Value = 100;
+        }
+    }
+}
