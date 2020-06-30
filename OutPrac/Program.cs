3f93a0c8d366ecc4bbadd4e63c24def78e3498ba
=devSOWON0628 <=s2018s29@e-mirim.hs.kr>
2020-06-30 14:26:43 +0900

    초기 커밋.

---
+++ OutPrac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OutPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사용자 입력 :");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(),out output);
+            if (result)
+            {
+                Console.WriteLine("값", output);
+            }
+            else
+            {
+                Console.WriteLine("정수를 입력하세요");
+            }
+        }
+    }
+}
